0efb626584c9d41acefc07cd4011f707a9f64265
Helge <helge.heuer@ds.mpg.de>
2021-10-02 12:18:19 +0200

    Mainly Data Analysis Updates

--- scenarios/Manhatten/Create-Input.sh
+++ scenarios/Manhatten/Create-Input.sh
@@ -14,7 +14,7 @@ meanDist="3000"
 seed="42"
 endTime="36000"
 departureIntervalTime="900"
-outputDir="900Mu0.77Zeta3Alpha"
+outputDir="900Mu0.77Zeta3AlphaNewStartLinks720Constr"
 
 #for SGE_TASK_ID in $(seq 1 40); do
 
@@ -26,8 +26,8 @@ ptSpacing=${ptSpacingList[$idx]}
 mode="create-input"
 
 dCut=$(echo "0.77 * $ptSpacing / 1" | bc)
-args="config.xml $mode $meanDist $dCut $ptSpacing $Ndrt $nReqs $seed $endTime $departureIntervalTime $outputDir"
+args="config.xml $mode $meanDist $dCut $ptSpacing $Ndrt $nReqs $seed $endTime $departureIntervalTime NaN $outputDir"
 echo $args
 #done
 
-/scratch01.local/hheuer/jdk-11.0.2/bin/java -Xmx6g -jar /scratch01.local/hheuer/matsim-bimodal-InverseGamma-OSM-1.0-SNAPSHOT-jar-with-dependencies.jar $args
+/scratch01.local/hheuer/jdk-11.0.2/bin/java -Xmx6g -jar /scratch01.local/hheuer/matsim-bimodal-InverseGamma3-OSM-1.0-SNAPSHOT-jar-with-dependencies.jar $args
